8f54cdcf6caf066aa6a0ef3fb4a68665c91dcf57
Leopard-C <leopard.c@outlook.com>
2023-04-17 22:25:33 +0800

    fix errors of build

--- xmake.lua
+++ xmake.lua
@@ -4,17 +4,18 @@ set_languages("c99", "cxx11")
 add_cxflags("-Wreturn-type", "-Wsign-compare", "-Wunused", "-Wswitch")
 add_cxflags("-Wno-deprecated-declarations")
 
+add_includedirs("include")
+add_linkdirs("lib/linux")
+
 target("iclient")
     set_kind("static")
-    add_includedirs("include")
     add_files("src/*.cpp")
+    add_links("curl", "pthread", "dl")
     set_targetdir("lib/linux")
 
 target("example.out")
     set_kind("binary")
-    add_includedirs("include")
     add_files("example/*.cpp")
-    add_linkdirs("lib/linux")
-    add_links("curl", "pthread", "dl", "iclient")
+    add_deps("iclient")
     set_targetdir("bin")
 
